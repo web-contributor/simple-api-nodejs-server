6a9e47d48b9acb70fc8d09428d3db1ef88502592
woon <sherman@mobilehead.com>
2024-10-07 10:22:14 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 var express = require('express');
 var app = express();
-var db = require('./db');
 global.__root   = __dirname + '/'; 
 
 app.get('/api', function (req, res) {
   res.status(200).send('API works.');
 });
 
-var UserController = require(__root + 'user/UserController');
-app.use('/api/users', UserController);
+var ImmoTestController = require(__root + 'immo-test');
+app.use('/api/immo', ImmoTestController);
 
-var AuthController = require(__root + 'auth/AuthController');
-app.use('/api/auth', AuthController);
+app.all('*', function (req, res) {
+  res.send("Server working well!");
+});
 
 module.exports = app;
